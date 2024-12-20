90e80b12d9b1410b52055959993f2bc714cb8f74
marcovcode <marcov.code@gmail.com>
2024-12-20 09:41:59 +0100

    Implement player movement

---
+++ scripts/player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+
+const SPEED = 100
+
+
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_axis("move_left", "move_right")
+
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
